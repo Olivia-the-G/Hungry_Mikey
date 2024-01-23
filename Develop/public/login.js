760b51c090425993f0b0a51dceda696648df3e84
ae-andre <aendique@gmail.com>
2024-01-22 19:29:26 -0700

    added login/logout functionality

---
+++ Develop/public/login.js
@@ -0,0 +1,53 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect userinput from login form
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      // send post request to API endpoint
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // Redirect to the profile page
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-signup').value.trim();
+    const user_password = document.querySelector('#password-signup').value.trim();
+  
+    if (email && user_password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ email, user_password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+  
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
+  
