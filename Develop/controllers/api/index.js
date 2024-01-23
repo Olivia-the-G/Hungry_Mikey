7a92863d51c46eaec952432aa5990905a84e1015
Flesh <olivia.a.gowan@gmail.com>
2024-01-22 20:07:06 -0500

    Code for assembly

--- Develop/controllers/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const userDataRoutes = require('/userData.js');
-
-router.use('/userData', userDataRoutes);
-
-module.exports = router;
--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
-const routes = require('./routes');
+// const apiRoutes = require('./api');
+const routes = require('./userRoutes');
 
 router.use('/', routes);
-router.use('/api', apiRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;
