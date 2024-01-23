61f9bf6ce669b8fae381445447727dd5ec628b64
Temp-JohnM89 <john.macneil89@gmail.com>
2024-01-22 21:43:52 -0400

    fixed const in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -5,6 +5,8 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const Sequelize = require('./config/connection');
 const brain = require('brain.js');
+const mysql = require('mysql2');
+const router = require('express').Router();
 
 
 
