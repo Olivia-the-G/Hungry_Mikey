4fe5ffee2982a6167c93904bc78f4779e9237b3c
ae-andre <aendique@gmail.com>
2024-01-22 19:31:30 -0700

    added login/logout functionality

--- Develop/server.js
+++ Develop/server.js
@@ -265,16 +265,6 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
-// Dashboard route - Protected
-router.get('/main', (req, res) => {
-  if (req.session.loggedin) {
-    res.send(`${randomMickeyGreetingAudio}`); // need to set up var for pulling random audio rec greeting
-  } else {
-    res.send('Please login to view this page!');
-  }
-  res.end();
-});
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
