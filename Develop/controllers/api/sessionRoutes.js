4c8d1279d15a9466584c174749935d20ac68ad55
ae-andre <aendique@gmail.com>
2024-01-24 19:57:24 -0700

    set up routes, models, and reorganized code

---
+++ Develop/controllers/api/sessionRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { Session } = require('../../models');
+
+
+  
+module.exports = router;
--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -1,9 +1,95 @@
-// Dashboard route - Protected
-router.get('/main', (req, res) => {
-    if (req.session.loggedin) {
-      res.send(`${randomMickeyGreetingAudio}`); // need to set up var for pulling random audio rec greeting
-    } else {
-      res.send('Please login to view this page!');
-    }
-    res.end();
-  });
+const router = require('express').Router();
+const { Project, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// http://localhost:3001/
+router.get('/', async (req, res) => {
+  try {
+    // // Get all projects and JOIN with user data
+    // const projectData = await Project.findAll({
+    //   include: [
+    //     {
+    //       model: User,
+    //       attributes: ['name'],
+    //     },
+    //   ],
+    // });
+
+    // Serialize data so the template can read it
+    // const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      // projects, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//http://localhost:3001/parent/1
+router.get('/parent/:id', async (req, res) => {
+  try {
+    const parentData = await Project.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const parent = parentData.get({ plain: true });
+
+    res.render('parent', {
+      ...parent,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//http://localhost:3001/profile
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
+
+
+// router.get('/main', (req, res) => {
+//     if (req.session.loggedin) {
+//       res.send(`${randomMickeyGreetingAudio}`); // need to set up var for pulling random audio rec greeting
+//     } else {
+//       res.send('Please login to view this page!');
+//     }
+//     res.end();
+//   });
